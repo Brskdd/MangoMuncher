f2c3c49b9936b9215f7f6202e19a8c852416c9f7
BroskiDood29 <yeetboi1229+1@gmail.com>
2023-06-02 09:58:37 -0400

    updated todo and set up task form

--- server.js
+++ server.js
@@ -113,7 +113,7 @@ app.get("/register", (req, res) => {
     res.sendFile(filepath);
 });
 app.post("/addtask/submit", (req, res) => {
-    res.send("add task form sent");
+    res.send(req.body.addtaskname);
     //do code for when user wants to add task
 });
 app.get("/login", (req, res) => {
